fd97b67e73c8aa46130282ad2aadd4ea14a6429c
Vahid Sabouhi <vahidsabouhi@yahoo.com>
2017-08-05 13:38:41 +0430

    action

--- app/src/main/java/com/example/vm2team/brawlstarsguide/wlc.java
+++ app/src/main/java/com/example/vm2team/brawlstarsguide/wlc.java
@@ -1,7 +1,9 @@
 package com.example.vm2team.brawlstarsguide;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class wlc extends AppCompatActivity {
 
